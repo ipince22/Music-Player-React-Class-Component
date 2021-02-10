896c912a43a9e88a8b622849de467735ae2d9723
Daniel Ipince <59460448+ipince22@users.noreply.github.com>
2021-02-10 16:40:03 -0500

    Create design.js
    probar design

---
+++ src/js/component/design.js
@@ -0,0 +1,36 @@
+//reemplazar en home para probar--
+//------------------------------------------
+import React from "react";
+//create your first component
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.audio = null; //will not change on reload. prefered: more flexible than method above
+		this.state = {};
+	}
+
+	render() {
+		const audioPlayer = (
+			<>
+				<div className="audioPlayer">
+					<button>
+						<i className="fa fa-caret-left" aria-hidden="true" />
+					</button>
+					<button>
+						<i className="fa fa-play" aria-hidden="true" />
+					</button>
+					<button>
+						<i className="fa fa-pause" aria-hidden="true" />
+					</button>
+					<button>
+						<i className="fa fa-caret-right" aria-hidden="true" />
+					</button>
+				</div>
+
+				<audio />
+			</>
+		);
+
+		return <>{audioPlayer}</>;
+	}
+}
